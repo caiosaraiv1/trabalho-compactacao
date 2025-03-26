bd634480acb6b60d8ea0ffa77a61674a27777db7
Kaique <165669385+kaiqpaiva@users.noreply.github.com>
2025-03-26 16:59:52 +0000

    tá quase terminando guys!!!

--- ex3.c
+++ ex3.c
@@ -2,6 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+void compactar(char *lettEntrada, char *lettSaida){
+    int tamanho = strlen(lettEntrada);
+    int i = 0;
+    int contador = 1;
+    char *ptr = lettSaida;
+    while(lettEntrada[i] != '\0'){
+        *ptr = lettEntrada[i];
+        while (i + 1 < strlen && lettEntrada[i] == lettEntrada[i + 1]){
+            i++;
+            contador++;
+        }
+        if(contador > 1){
+            ptr += sprintf(ptr, "%d", contador);
+        }
+        if(i + 1 < strlen && lettEntrada[i] == lettEntrada[i + 1 - contador]){
+            *ptr = '-';
+        }
+    }   
+}
+
+
 int main(){
     char entrada[100];
     char saida[100];
