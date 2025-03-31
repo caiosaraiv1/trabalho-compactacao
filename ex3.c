0f7ffa04a46989be71ece9d3d199a20feff4b69e
kaiqpaiva <kaique.paiva681@gmail.com>
2025-03-30 21:17:14 -0300

    final ex 3

--- ex3.c
+++ ex3.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define TAMANHO 100
+#define TAMANHO 100 // define o tamanho maximo pra usar nas strings
+
+// Encontra caractere igual e manda pra saida
 int repeat(char lettEntrada[], char lettSaida[], int inicio, int size){
 
     int firstLett = lettEntrada[inicio];
     int  lastSet = inicio + 1;
-    
+
     while (lastSet< size && lettEntrada[lastSet] != firstLett) {
         lastSet++;
     }
 
     int rep = lastSet - inicio;
-    strncpy(lettSaida, lettEntrada + inicio, rep); // copia o grupo contiguo para a variavel
-    lettSaida[rep] = '\0'; // adiciona o fim da string
+    strncpy(lettSaida, lettEntrada + inicio, rep); 
+    lettSaida[rep] = '\0'; 
     
-    return lastSet;
+    return lastSet; // retorna aonde vai iniciar a proxima busca
 }
-
+ // conta quantas vezes tem repeticao
 int cont_Rep(char lettEntrada[], char lettSaida[], int *inicio, int size){
+
     int repet = 1;
     int saida_size = strlen(lettSaida);
 
@@ -35,12 +38,13 @@ int main(){
     char entrada[TAMANHO];
     char saida[TAMANHO];
     int inicio = 0;
-
+    
     printf("Digite ai: ");
     fgets(entrada, TAMANHO, stdin);
-    entrada[strcspn(entrada, "\n")] = '\0'; 
-    
+    entrada[strcspn(entrada, "\n")] = '\0';   // remove o \n
+
     int tamanho = strlen(entrada);
+
     while(inicio < tamanho){
         int inicio_indice = repeat(entrada, saida, inicio, tamanho);
         int repeticao = cont_Rep(entrada, saida, &inicio_indice, tamanho);
@@ -49,6 +53,5 @@ int main(){
         inicio = inicio_indice;
 
         (inicio< tamanho) && printf("-");
-
     }
 }
