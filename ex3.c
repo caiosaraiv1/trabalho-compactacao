ad5b0adc04a6f4d1397c0c8b69f55bfab5a4d8d2
kaiqpaiva <kaique.paiva681@gmail.com>
2025-03-30 21:05:44 -0300

    finalizado

--- ex3.c
+++ ex3.c
@@ -1,33 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define TAMANHO 100
+int repeat(char lettEntrada[], char lettSaida[], int inicio, int size){
 
-void compactar(char *lettEntrada, char *lettSaida){
-    int tamanho = strlen(lettEntrada);
-    int i = 0;
-    int contador = 1;
-    char *ptr = lettSaida;
-    while(lettEntrada[i] != '\0'){
-        *ptr = lettEntrada[i];
-        while (i + 1 < strlen && lettEntrada[i] == lettEntrada[i + 1]){
-            i++;
-            contador++;
-        }
-        if(contador > 1){
-            ptr += sprintf(ptr, "%d", contador);
-        }
-        if(i + 1 < strlen && lettEntrada[i] == lettEntrada[i + 1 - contador]){
-            *ptr = '-';
-        }
-    }   
+    int firstLett = lettEntrada[inicio];
+    int  lastSet = inicio + 1;
+    
+    while (lastSet< size && lettEntrada[lastSet] != firstLett) {
+        lastSet++;
+    }
+
+    int rep = lastSet - inicio;
+    strncpy(lettSaida, lettEntrada + inicio, rep); // copia o grupo contiguo para a variavel
+    lettSaida[rep] = '\0'; // adiciona o fim da string
+    
+    return lastSet;
 }
 
+int cont_Rep(char lettEntrada[], char lettSaida[], int *inicio, int size){
+    int repet = 1;
+    int saida_size = strlen(lettSaida);
+
+    if(saida_size == 0) return 0;
+
+    while(*inicio + saida_size <= size && strncmp(&lettEntrada[*inicio], lettSaida, saida_size) == 0){
+        repet++;
+        *inicio += saida_size;
+    }
+    return repet ;
+}
 
 int main(){
-    char entrada[100];
-    char saida[100];
+    char entrada[TAMANHO];
+    char saida[TAMANHO];
+    int inicio = 0;
 
     printf("Digite ai: ");
-    fgets(entrada, 100, stdin);
+    fgets(entrada, TAMANHO, stdin);
     entrada[strcspn(entrada, "\n")] = '\0'; 
+    
+    int tamanho = strlen(entrada);
+    while(inicio < tamanho){
+        int inicio_indice = repeat(entrada, saida, inicio, tamanho);
+        int repeticao = cont_Rep(entrada, saida, &inicio_indice, tamanho);
+
+        printf("%s%d", saida, repeticao);
+        inicio = inicio_indice;
+
+        (inicio< tamanho) && printf("-");
+
+    }
 }
